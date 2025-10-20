b415b71112759def3137938e36ba9cf8128b9f43
Pol <pol.gil.figuerola@upc.edu>
2025-10-20 22:51:44 +0200

    replaced  'fair share' for 'members' and normalize in the pipeline (validated)

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,11 +1,13 @@
 # From the repository root
 for scenario in \
+  scenarios/ds-a_dials-balanced.yaml \
+  scenarios/ds-a_dials-efficiency.yaml \
+  scenarios/ds-a_dials-fairness.yaml \
   scenarios/ds-a_dials-hard-fairness.yaml \
-  #scenarios/ds-a_dials-balanced.yaml \
-  #scenarios/ds-a_dials-efficiency.yaml \
-  #scenarios/ds-a_dials-fairness.yaml \
   #scenarios/ds-b_dials-balanced.yaml \
   #scenarios/ds-b_dials-efficiency.yaml \
+  #scenarios/ds-b_dials-fairness.yaml \
+  #scenarios/ds-b_dials-hard-fairness.yaml \
   do
     python -m fbdam.engine.run run "$scenario" --profile time-limited
     # python -m fbdam.engine.run run "$scenario" --profile gap-limited
