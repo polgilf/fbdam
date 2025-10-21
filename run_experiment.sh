fab6b5945e4b6d47039fd1a7701e328f5d13e8ee
Pol <pol.gil.figuerola@upc.edu>
2025-10-21 17:38:10 +0200

    preparing definitive experiments for presentation

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,10 +1,10 @@
 # From the repository root
 for scenario in \
-  scenarios/ds-a_dials-balanced.yaml \
   scenarios/ds-a_dials-efficiency.yaml \
-  scenarios/ds-a_dials-fairness.yaml \
   scenarios/ds-a_dials-adequacy.yaml \
-  scenarios/ds-a_dials-fairness-adequacy.yaml \
+  #scenarios/ds-a_dials-balanced.yaml \
+  #scenarios/ds-a_dials-fairness.yaml \
+  #scenarios/ds-a_dials-fairness-adequacy.yaml \
   #scenarios/ds-a_dials-hard-fairness.yaml \
   #scenarios/ds-b_dials-balanced.yaml \
   #scenarios/ds-b_dials-efficiency.yaml \
